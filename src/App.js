ba35fae32fcd52f5916f9963f8aeb12fb21c2daf
rasgrin <stefanrogic@protonmail.com>
2023-02-07 21:56:00 +0100

    removed router for now - don't work with gh pages

--- src/App.js
+++ src/App.js
@@ -3,23 +3,29 @@ import Random from "./pages/random/Random";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 
 function App() {
-  const Layout = () => {
-    return (
-      <div className="App">
-        <Outlet />
-      </div>
-    );
-  };
+  // const Layout = () => {
+  //   return (
+  //     <div className="App">
+  //       <Outlet />
+  //     </div>
+  //   );
+  // };
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Layout />,
-      children: [{ path: "/", element: <Random /> }],
-    },
-  ]);
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element: <Layout />,
+  //     children: [{ path: "/", element: <Random /> }],
+  //   },
+  // ]);
 
-  return <RouterProvider router={router}></RouterProvider>;
+  // return <RouterProvider router={router}></RouterProvider>;
+
+  return (
+    <div className="App">
+      <Random />
+    </div>
+  );
 }
 
 export default App;
