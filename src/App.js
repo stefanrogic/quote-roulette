3e51846b7f9d52eed5cbe5c07395966115339ae1
rasgrin <stefanrogic@protonmail.com>
2023-07-25 14:19:06 +0200

    removed navbar for now and refactored css

--- src/App.js
+++ src/App.js
@@ -1,46 +1,12 @@
-import Nav from "./components/nav/Nav";
 import Random from "./pages/random/Random";
 
-import Search from "./components/search/Search";
-import { useState } from "react";
 import { useGetRandomQuote } from "./hooks/useGetRandomQuote";
 
-// import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
-
 function App() {
-  const [showSearch, setShowSearch] = useState(false);
   const { quote, isLoading, newQuote, newAuthorQuote, setIsLoading, newCategoryQuote } = useGetRandomQuote();
-
-  // const Layout = () => {
-  //   return (
-  //     <div className="App">
-  //       <Outlet />
-  //     </div>
-  //   );
-  // };
-
-  // const router = createBrowserRouter([
-  //   {
-  //     path: "/",
-  //     element: <Layout />,
-  //     children: [{ path: "/", element: <Random /> }],
-  //   },
-  // ]);
-
-  // return <RouterProvider router={router}></RouterProvider>;
-
-  const searchModalSwitch = () => {
-    setShowSearch((prev) => !prev);
-  };
-
-  const hideSearchModal = () => {
-    setShowSearch(false);
-  };
 
   return (
     <div className="App">
-      <Nav switchModal={searchModalSwitch} />
-      {showSearch && <Search onHideModal={hideSearchModal} onSetIsLoading={setIsLoading} onNewAuthorQuote={newAuthorQuote} />}
       <Random onQuote={quote} onIsLoading={isLoading} onSetIsLoading={setIsLoading} onNewQuote={newQuote} onNewAuthorQuote={newAuthorQuote} onNewCategoryQuote={newCategoryQuote} />
     </div>
   );
--- src/pages/random/Random.jsx
+++ src/pages/random/Random.jsx
@@ -1,55 +1,79 @@
 import "./random.scss";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { ReactComponent as QuoteLogo } from "../../assets/img/icons/quote-icon.svg";
+import { AnimatePresence, animate, motion } from "framer-motion";
 
 const Random = ({ onQuote, onIsLoading, onNewQuote, onNewAuthorQuote, onSetIsLoading, onNewCategoryQuote }) => {
-  useEffect(() => onNewQuote(), []); // eslint-disable-line react-hooks/exhaustive-deps
+  const [firstVisit, setFirstVisit] = useState(localStorage.getItem("firstVisit") || true);
+
+  useEffect(() => onNewQuote(), []);
+
+  const handlePopup = () => {
+    setFirstVisit(false);
+    localStorage.setItem("firstVisit", JSON.stringify(false));
+  };
 
   return (
-    <div className="random">
-      <button
-        className="quote"
-        onClick={() => {
-          onSetIsLoading(true);
-          onNewQuote();
-        }}
-      >
-        <QuoteLogo fill={"#ededed"} />
-      </button>
+    <motion.div className="random" initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
       <div className="container">
-        {onIsLoading ? (
-          <span>
+        <motion.div className="top" initial={{ opacity: 0 }} animate={{ opacity: 1, scale: [1, 2, 1] }} transition={{ duration: 0.5 }}>
+          <AnimatePresence>
+            {JSON.parse(firstVisit) && (
+              <motion.div className="tip" initial={{ opacity: 0 }} animate={{ scale: [1, 2, 1], opacity: [0, 1, 1] }} exit={{ scale: 0, opacity: 0 }} transition={{ duration: 0.5, delay: 1.5 }} onClick={handlePopup}>
+                <div className="arrow"></div>
+                <p>Click to generate new quote</p>
+              </motion.div>
+            )}
+          </AnimatePresence>
+          <motion.button
+            className="quote"
+            onClick={() => {
+              onSetIsLoading(true);
+              onNewQuote();
+            }}
+            onMouseEnter={handlePopup}
+            whileHover={{ scale: 1.2 }}
+            whileTap={{ scale: 0.9 }}
+          >
             <QuoteLogo fill={"#ededed"} />
-            LOADING
-          </span>
-        ) : (
-          <>
-            <div className="content">
-              <p>"{onQuote.content}"</p>
-            </div>
+          </motion.button>
+        </motion.div>
 
-            <div className="author">
-              <div className="line" />
+        <AnimatePresence>
+          {onIsLoading ? (
+            <span>
+              <QuoteLogo fill={"#ededed"} />
+              LOADING
+            </span>
+          ) : (
+            <>
+              <motion.div className="content" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.5 }}>
+                <p>"{onQuote.content}"</p>
+              </motion.div>
 
-              <a href={`#${onQuote.authorSlug}`}>
-                <h1 onClick={(e) => onNewAuthorQuote(e.target.textContent)}>{onQuote.author.toUpperCase()}</h1>
-              </a>
+              <motion.div className="author" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.5, delay: 0.5 }}>
+                <div className="line" />
 
-              <div className="line" />
-            </div>
-
-            <div className="tags">
-              {onQuote.tags.map((tag, key) => (
-                <a key={key} href={`#${tag}`} onClick={(e) => onNewCategoryQuote(e.target.textContent.substring(1))}>
-                  #{tag}
+                <a href={`#${onQuote.authorSlug}`}>
+                  <h1 onClick={(e) => onNewAuthorQuote(e.target.textContent)}>{onQuote.author.toUpperCase()}</h1>
                 </a>
-              ))}
-            </div>
-          </>
-        )}
+
+                <div className="line" />
+              </motion.div>
+
+              <motion.div className="tags" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.5, delay: 1 }}>
+                {onQuote.tags.map((tag, key) => (
+                  <a key={key} href={`#${tag}`} onClick={(e) => onNewCategoryQuote(e.target.textContent.substring(1))}>
+                    #{tag}
+                  </a>
+                ))}
+              </motion.div>
+            </>
+          )}
+        </AnimatePresence>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
